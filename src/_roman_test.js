53858065c3c88288f038dc6c88dab2909cad5e32
davidbaldwin <david.baldwin@gmsl.co.uk>
2012-09-13 16:16:57 +0100

    Refactored tests and code to provide sync and async versions

--- src/_roman_test.js
+++ src/_roman_test.js
@@ -1,113 +1,58 @@
 "use strict";
 
 var roman = require("./roman.js");
+var async= require("async");
+
+var testCases = [
+    [2367,'MMCCCLXVII'],
+    [20,'XX'],
+    [19,'XIX'],
+    [18,'XVIII'],
+    [17,'XVII'],
+    [16,'XVI'],
+    [15,'XV'],
+    [14,'XIV'],
+    [13,'XIII'],
+    [12,'XII'],
+    [11,'XI'],
+    [10,'X'],
+    [9,'IX'],
+    [8,'VIII'],
+    [7,'VII'],
+    [6,'VI'],
+    [5,'V'],
+    [4,'IV'],
+    [3,'III'],
+    [2,'II'],
+    [1,'I']
+];
 
 
-exports.test_Converts1ToI = function(test) {
-	roman.convertToRomanNumerals(1, function(numeral) {
-		test.equal(numeral, "I");
-		test.done();
-	});
-};
+exports.test_syncConverts = function(test) {
+    //Synchronously
+    for (var i=0; i<testCases.length; i++) {
+        //console.log('test ' + testCases[i][0] + ' converts to ' + testCases[i][1]);
+        test.equal(roman.syncConvertToRomanNumerals(testCases[i][0]), testCases[i][1], "Number:" + testCases[i][0]);
+    }
+    test.done();
 
-exports.test_Converts2ToII = function(test) {
-	roman.convertToRomanNumerals(2, function(numeral) {
-		test.equal(numeral, "II");
-		test.done();
-	});
-};
-
-exports.test_Converts3ToIII = function(test) {
-	roman.convertToRomanNumerals(3, function(numeral) {
-		test.equal(numeral, "III");
-		test.done();
-	});
-};
-
-
-exports.test_Converts5ToV = function(test) {
-	roman.convertToRomanNumerals(5, function(numeral) {
-		test.equal(numeral, "V");
-		test.done();
-	});
-};
-
-exports.test_Converts6ToVI = function(test) {
-	roman.convertToRomanNumerals(6, function(numeral) {
-		test.equal(numeral, "VI");
-		test.done();
-	});
-};
-
-exports.test_Converts7ToVII = function(test) {
-	roman.convertToRomanNumerals(7, function(numeral) {
-		test.equal(numeral, "VII");
-		test.done();
-	});
-};
-
-exports.test_Converts8ToVIII = function(test) {
-	roman.convertToRomanNumerals(8, function(numeral) {
-		test.equal(numeral, "VIII");
-		test.done();
-	});
-};
-
-exports.test_Converts10ToX = function(test) {
-	roman.convertToRomanNumerals(10, function(numeral) {
-		test.equal(numeral, "X");
-		test.done();
-	});
-};
-
-exports.test_Converts11ToXI = function(test) {
-	roman.convertToRomanNumerals(11, function(numeral) {
-		test.equal(numeral, "XI");
-		test.done();
-	});
-};
-
-exports.test_Converts50ToL = function(test) {
-	roman.convertToRomanNumerals(50, function(numeral) {
-		test.equal(numeral, "L");
-		test.done();
-	});
-};
-
-exports.test_Converts100ToC = function(test) {
-	roman.convertToRomanNumerals(100, function(numeral) {
-		test.equal(numeral, "C");
-		test.done();
-	});
-};
-
-exports.test_Converts1000ToM = function(test) {
-	roman.convertToRomanNumerals(1000, function(numeral) {
-		test.equal(numeral, "M");
-		test.done();
-	});
-};
-
-exports.test_Converts2367ToMMCCCLXVII = function(test) {
-	roman.convertToRomanNumerals(2367, function(numeral) {
-		test.equal(numeral, "MMCCCLXVII");
-		test.done();
-	});
 };
 
 
+exports.test_asyncConverts = function(test) {
+    //Asynchronously
+    var testInputs = [];
+    var testOutputs = [];
 
-exports.test_Converts4ToIV = function(test) {
-	roman.convertToRomanNumerals(4, function(numeral) {
-		test.equal(numeral, "IV");
-		test.done();
-	});
-};
+    for (var i=0; i<testCases.length; i++) {
+        testInputs[i] = testCases[i][0];
+        testOutputs[i] = testCases[i][1];
+    }
 
-
-exports.test_Converts9ToIX = function(test) {
-	roman.convertToRomanNumerals(9, function(numeral) {
-		test.equal(numeral, "IX");
-		test.done();
-	});
+    async.map(testInputs, roman.convertToRomanNumerals, function(err, results) {
+        for (var i=0; i<testInputs.length; i++) {
+            test.equal(results[i], testOutputs[i], "Number:" + testInputs[i]);
+        }
+        test.done();
+    });
 };
--- src/roman.js
+++ src/roman.js
@@ -6,58 +6,67 @@ String.prototype.repeat = function(n) {
     return new Array(1 + n).join(this);
 };
 
-function convertToChar(number, unit, char, nextUnitUp, nextCharUp, callback) {
+function convertToChar(number, unit, unitChar, unit5Char, unit10Char, callback) {
 
-//console.log("N:" + number + " U:" + unit + " C:" + char);
+    var digit = ((number % (unit * 10)) - (number % unit)) / unit;
 
-	var mod = number % nextUnitUp;
-	var rem = mod % unit;
-	var quot = (mod - rem) / unit;
-
-	if (quot === nextUnitUp - 1) {
-		callback(char + nextCharUp);
-	} else {
-		callback(char.repeat(quot));
-	}
-	
+    if (digit === 9) {
+        callback(null, unitChar + unit10Char);
+    } else if (digit >= 5) {
+        callback(null, unit5Char + unitChar.repeat(digit % 5));
+    } else if (digit === 4) {
+        callback(null, unitChar + unit5Char);
+    } else {
+        callback(null, unitChar.repeat(digit));
+    }
 }
 
 exports.convertToRomanNumerals = function(number, callback) {
 
 	async.parallel([
-		function(callback){
-			convertToChar(number, 1000, "M", 10000, "Z", function(text) {
-				callback(null, text);
-			});
-		},
-		function(callback){
-			convertToChar(number % 1000, 100, "C", 1000, "M", function(text) {
-				callback(null, text);
-			});
-		},
-		function(callback){
-			convertToChar(number % 100, 50, "L", 100, "C", function(text) {
-				callback(null, text);
-			});
-		},
-		function(callback){
-			convertToChar(number % 50, 10, "X", 50, "L", function(text) {
-				callback(null, text);
-			});
-		},
-		function(callback){
-			convertToChar(number % 10, 5, "V", 10, "X", function(text) {
-				callback(null, text);
-			});
-		},
-		function(callback){
-			convertToChar(number % 5, 1, "I", 5, "V", function(text) {
-				callback(null, text);
-			});
-		}
+        function(callback){
+            convertToChar(number, 1000, "M", "F", "S", function(err, results) {
+                callback(err, results);
+            });
+        },
+        function(callback){
+            convertToChar(number, 100, "C", "D", "M", function(err, results) {
+                callback(err, results);
+            });
+        },
+        function(callback){
+            convertToChar(number, 10, "X", "L", "C", function(err, results) {
+                callback(err, results);
+            });
+        },
+        function(callback){
+            convertToChar(number, 1, "I", "V", "X", function(err, results) {
+                callback(err, results);
+            });
+        }
 	], function (err, result) {
-		callback(result.join(""));
+
+        callback(err, result.join(""));
 	});
 
+};
+
+function syncConvertToChar(number, unit, unitChar, unit5Char, unit10Char) {
+
+    var digit = ((number % (unit * 10)) - (number % unit)) / unit;
+
+    if (digit === 9) {
+        return unitChar + unit10Char;
+    } else if (digit >= 5) {
+        return unit5Char + unitChar.repeat(digit % 5);
+    } else if (digit === 4) {
+        return unitChar + unit5Char;
+    } else {
+        return unitChar.repeat(digit);
+    }
+}
+
+exports.syncConvertToRomanNumerals = function(number) {
+    return syncConvertToChar(number, 1000, "M", "F", "S") + syncConvertToChar(number, 100, "C", "D", "M") + syncConvertToChar(number, 10, "X", "L", "C") + syncConvertToChar(number, 1, "I", "V", "X");
 };
 
